834ebd0ab71236fa4909b526879f6347b141a462
georgecherry02 <georgeb.cherry@gmail.com>
2023-09-16 16:43:46 +0100

    Add basic test for visitor

--- tests/multi_invoke_visitor.t.cpp
+++ tests/multi_invoke_visitor.t.cpp
@@ -2,15 +2,42 @@
 
 #include <multi_invoke_visitor.h>
 
-SCENARIO("Example Scenario")
+struct TestStruct
 {
-    GIVEN("Example Given")
+    int value;
+};
+
+SCENARIO("Basic usage of the visitor")
+{
+    GIVEN("A dispatcher with 3 unique signatures")
     {
-        WHEN("Example When")
+        bool one_called, two_called, three_called;
+        auto dispatch = snippets::make_dispatcher(
+            [&one_called](std::string_view s) {
+                one_called = true;
+                CHECK(s == "Test string view");
+            },
+            [&two_called](std::string_view s, int i) {
+                two_called = true;
+                CHECK(s == "Test string view");
+                CHECK(i == 5);
+            },
+            [&three_called](std::string_view s, int i, TestStruct ts) {
+                three_called = true;
+                CHECK(s == "Test string view");
+                CHECK(i == 5);
+                CHECK(ts.value == 5);
+            });
+        WHEN("The dispatcher is called on three occasions with these different signatures")
         {
-            THEN("Example Then")
+            dispatch("Test string view");
+            dispatch("Test string view", 5);
+            dispatch("Test string view", 5, TestStruct{5});
+            THEN("Then the outside state flags have been modified")
             {
-                CHECK(5 == 5);
+                CHECK(one_called);
+                CHECK(two_called);
+                CHECK(three_called);
             }
         }
     }
