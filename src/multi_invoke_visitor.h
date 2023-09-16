c1a51d96950d9de23718c730210aabe270733ad3
georgecherry02 <georgeb.cherry@gmail.com>
2023-09-16 14:57:43 +0100

    Added a helper function for construction

--- src/multi_invoke_visitor.h
+++ src/multi_invoke_visitor.h
@@ -16,11 +16,24 @@ struct is_variant<std::variant<V...>> : std::true_type
 {
 };
 
+template <typename... Handlers>
 class MultiInvokeVisitor
 {
+  private:
+    std::tuple<Handlers...> d_handlers;
+
   public:
+    explicit MultiInvokeVisitor(Handlers... handlers)
+        : d_handlers{std::move(handlers)...} {};
+
     int stub();
 };
+
+template <typename... Handlers>
+constexpr decltype(auto) make_dispatcher(Handlers&&... handlers)
+{
+    return MultiInvokeVisitor<std::decay_t<Handlers>...>{std::forward<Handlers>(handlers)...};
+}
 
 } // namespace snippets
 
