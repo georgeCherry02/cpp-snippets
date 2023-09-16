48037c0a994a219adec35c78de78e163eee31af0
georgecherry02 <georgeb.cherry@gmail.com>
2023-09-16 15:18:51 +0100

    Add ability to expand a variant and curry with the stored alternative

--- src/multi_invoke_visitor.h
+++ src/multi_invoke_visitor.h
@@ -1,6 +1,7 @@
 #ifndef INCLUDED_MULTI_INVOKE_VISITOR_H
 #define INCLUDED_MULTI_INVOKE_VISITOR_H
 
+#include <tuple>
 #include <utility>
 #include <variant>
 
@@ -39,7 +40,7 @@ class MultiInvokeVisitor
         if constexpr (is_variant_v<std::decay_t<FIRST>>)
         {
             // Visit the variant to unfold
-            return curry_variant(std::forward<FUNC>(func), std::forward<FIRST>(first), std::forward<REST>(rest)...);
+            return expand_variant(std::forward<FUNC>(func), std::forward<FIRST>(first), std::forward<REST>(rest)...);
         }
 
         // Curry the func with the first argument and then use tail recursion
@@ -54,6 +55,28 @@ class MultiInvokeVisitor
                 }
             },
             std::forward<REST>(rest)...);
+    }
+
+    template <typename FUNC, typename VARIANT, typename... REST>
+    constexpr void expand_variant(FUNC&& func, VARIANT&& variant, REST&&... rest)
+    {
+        // We can use a lambda accepting auto&& as regardless of the alternative
+        // contained within the variant we want to call the curring function with
+        // it
+        std::visit(
+            [&func, rest_tup = std::forward_as_tuple(std::forward<REST>(rest)...), this](auto&& alt) {
+                // The `std::apply` is necessary because you can't capture parameter packs until C++ 23 therefore you
+                // have to forward as tuple and expand...
+                std::apply(
+                    [&func, &alt, this](auto&&... r) {
+                        curry(
+                            std::forward<FUNC>(func),
+                            std::forward<decltype(alt)>(alt),
+                            std::forward<decltype(rest)>(r)...);
+                    },
+                    rest_tup);
+            },
+            std::forward<VARIANT>(variant));
     }
 
   public:
